94e5949dc6f5488f1d9ade680d9d0b93ef242725
nashihu <ahmad.n.id@gmail.com>
2022-11-09 10:00:05 +0700

    init

--- stopandwait.c
+++ stopandwait.c
@@ -16,7 +16,6 @@
 // #define PORT_NO 15050
 // #define IP_ADDRESS "127.0.0.1" // localhost
 #define NET_BUF_SIZE 2048
-#define cipherKey 'S'
 #define sendrecvflag 0
 #define nofile "File Not Found!"
 
@@ -28,11 +27,12 @@ socklen_t adrlencli = sizeof(addr_cli);
 
 int x = 0;
 
-typedef struct frame {
+typedef struct frame
+{
     unsigned char *pbuf;
     int ack;
     int length;
-}Frame;
+} Frame;
 
 // function to clear buffer
 void clearBufs(char *b)
@@ -49,54 +49,6 @@ int mins(int a, int b)
     return b;
 }
 
-// function to encrypt
-char cipher(char ch)
-{
-    return ch;
-}
-
-int send_data(int sock, void *buf, int buflen)
-{
-    Frame frame_send;
-    unsigned char *pbuf = (unsigned char *)buf;
-
-    int num;
-
-    while (buflen > 0)
-    {
-        frame_send.pbuf = pbuf;
-        frame_send.length = buflen;
-        num = sendto(sock, &frame_send, sizeof(Frame),
-                         sendrecvflag, (struct sockaddr *)&addr_cli,
-                         adrlencli);
-        // num = sendto(sock, pbuf, buflen,
-        //                  sendrecvflag, (struct sockaddr *)&addr_cli,
-        //                  adrlencli);
-                         printf("sent %d %d\n", ++x, buflen);
-        if (num < 1)
-        {
-            perror("error send");
-            return 0;
-        }
-
-        pbuf += buflen;
-        buflen -= buflen;
-    }
-
-    return 1;
-}
-
-int sendzero(int sock, long value)
-{
-    Frame frame_zero;
-    frame_zero.length = 0;
-    // value = htonl(value);
-    int num;
-    num = sendto(sock, &frame_zero, sizeof(Frame),
-                         sendrecvflag, (struct sockaddr *)&addr_cli,
-                         adrlencli);
-    return num;
-}
 
 int send_files(int sock, FILE *f)
 {
@@ -107,16 +59,20 @@ int send_files(int sock, FILE *f)
         return 0;
     // if (!sendzero(sock, filesize))
     //     return 0;
+    printf("FILE SIZEW %lu\n", filesize);
     if (filesize > 0)
     {
-        char buffer[NET_BUF_SIZE];
         do
         {
+            char buffer[NET_BUF_SIZE];
             size_t num = mins(filesize, sizeof(buffer));
             num = fread(buffer, 1, num, f);
             if (num < 1)
                 return 0;
-            if (!send_data(sock, buffer, num))
+            int res = sendto(sock, &buffer, num,
+                             sendrecvflag, (struct sockaddr *)&addr_cli,
+                             adrlencli);
+            if (!res)
             {
                 return 0;
             }
@@ -124,70 +80,27 @@ int send_files(int sock, FILE *f)
             // printf("fsiz %ld\n", filesize);
         } while (filesize > 0);
     }
-    sendzero(sock, 0);
-    return 1;
-}
-
-
-int read_data(int sock, void *buf, int buflen)
-{
-    Frame frame_read;
-    unsigned char *pbuf = (unsigned char *)buf;
-
-    while (buflen > 0)
-    {
-        int fileSize;
-        // fileSize = recvfrom(sock, pbuf,
-        //                    NET_BUF_SIZE, sendrecvflag,
-        //                    (struct sockaddr *)&addr_con, &adrlen);
-        fileSize = recvfrom(sock, &frame_read,
-                           sizeof(Frame), sendrecvflag,
-                           (struct sockaddr *)&addr_con, &adrlen);
-        pbuf = frame_read.pbuf;
-        if (fileSize <= 0)
-        {
-            return 0;
-        }
-        fileSize = frame_read.length;
-        printf("dpt %d %d\n", ++x, fileSize);
-        if (fileSize <= 0)
-        {
-            return 0;
-        }
-        pbuf += fileSize;
-        buflen -= fileSize;
-    }
-
+    sendto(sock, NULL, 0,
+                 sendrecvflag, (struct sockaddr *)&addr_cli,
+                 adrlencli);
     return 1;
 }
 
 int read_file(int sock, FILE *f)
 {
-    long filesize = NET_BUF_SIZE;
-    if (filesize > 0)
+    int num = 0;
+    while (1)
     {
         char buffer[NET_BUF_SIZE];
-        do
+        num = recvfrom(sock, &buffer,
+                       NET_BUF_SIZE, sendrecvflag,
+                       (struct sockaddr *)&addr_con, &adrlen);
+        if (num == 0)
         {
-            int num = mins(filesize, sizeof(buffer));
-            if (!read_data(sock, buffer, num))
-                return 0;
-            int offset = 0;
-            do
-            {
-                size_t written = fwrite(&buffer[offset], 1, num - offset, f);
-                if (written < 1)
-                    return 0;
-                offset += written;
-            } while (offset < num);
-            // filesize -= num;
-            if (num < filesize)
-            {
-                filesize = num;
-            }
-        } while (filesize > 0);
-    }
-    return 1;
+            return 0;
+        }
+        fwrite(&buffer[0], 1, num, f);
+    };
 }
 
 int rdtServerFile(char *iface, long port, int use_udp, FILE *fp)
@@ -224,6 +137,7 @@ int rdtServerFile(char *iface, long port, int use_udp, FILE *fp)
     // receive file name
     clearBufs(net_buf);
     // long sip = 0;
+    // readzero(sockfd, &sip);
     read_file(sockfd, fp);
     fclose(fp);
     return 0;
@@ -255,16 +169,18 @@ int rdtClientFile(char *host, long port, int use_udp, FILE *fp)
     }
 
     clearBufs(net_buf);
-    send_files(sockfd, fp); //udp
+    send_files(sockfd, fp); // udp
     fclose(fp);
     // printf("\n-------------------------------\n");
     return 0;
 }
 
-void stopandwait_server(char* iface, long port, FILE* fp) {
-  rdtServerFile(iface, port, 1, fp);
+void stopandwait_server(char *iface, long port, FILE *fp)
+{
+    rdtServerFile(iface, port, 1, fp);
 }
 
-void stopandwait_client(char* host, long port, FILE* fp) {
+void stopandwait_client(char *host, long port, FILE *fp)
+{
     rdtClientFile(host, port, 1, fp);
 }
