711b5915ae760b01e30d07805694b7d7fb8fbdae
nashihu <ahmad.n.id@gmail.com>
2022-11-27 10:04:46 +0700

    gbn initial workd

---
+++ gobackn.c
@@ -0,0 +1,268 @@
+#include <stdio.h>
+#include <string.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include <signal.h>
+
+#define PORT 9000
+
+#define IP_PROTOCOL 0
+#define NET_BUF_SIZE 200
+#ifndef MSG_CONFIRM
+#define MSG_CONFIRM 0
+#endif
+
+// struct sockaddr_in serverAddr;
+// socklen_t serverAddrLen = sizeof(serverAddr);
+
+// struct sockaddr_in clientAddr;
+// socklen_t clientAddrLen = sizeof(clientAddr);
+
+typedef struct frame
+{
+    char buffer[NET_BUF_SIZE];
+    int ack;
+    int length;
+    int frame_kind;
+    int seq_num;
+    int check_sum;
+} Frame;
+
+// framekind
+#define ACK 0
+#define SEQ 1
+
+int sockfdc;
+int success;
+Frame fsend;
+Frame fresponse;
+void alarmHandlerGbn(int signum, struct sockaddr_in serverAddr)
+{
+    socklen_t serverAddrLen = sizeof(serverAddr);
+    alarm(1);
+    if (signum != 1)
+    {
+        printf("triggered out of 1 %d\n", signum);
+    }
+    int dapet = sendto(sockfdc, &fsend, sizeof(Frame),
+                       MSG_CONFIRM, (struct sockaddr *)&serverAddr,
+                       serverAddrLen);
+    printf("sent %d\n", dapet);
+    recvfrom(sockfdc, &fresponse, sizeof(Frame), MSG_CONFIRM,
+             (struct sockaddr *)&serverAddr, &serverAddrLen);
+    success = fresponse.ack == 1 && fresponse.frame_kind == ACK;
+}
+
+int _checksum(char buffer[NET_BUF_SIZE])
+{
+    int sum = 0;
+    for (int i = 0; i < NET_BUF_SIZE; i++)
+    {
+        sum += buffer[i];
+    }
+    return sum;
+}
+
+int _min(int a, int b)
+{
+    if (a < b)
+        return a;
+    return b;
+}
+
+int _send_files(int sock, FILE *f, struct sockaddr_in serverAddr)
+{
+    // sleep(2);
+    fseek(f, 0, SEEK_END);
+    long filesize = ftell(f);
+    rewind(f);
+    if (filesize == EOF)
+        return 0;
+
+    int step = 0;
+    if (filesize > 0)
+    {
+        do
+        {
+            memset(&fsend, 0, sizeof(fsend));
+            memset(&fresponse, 0, sizeof(fresponse));
+            success = 0;
+            int attempt = 0;
+            char buffer[NET_BUF_SIZE];
+            size_t num = _min(filesize, sizeof(buffer));
+            num = fread(fsend.buffer, 1, num, f);
+            fsend.length = num;
+            fsend.seq_num = step;
+            fsend.frame_kind = SEQ;
+            fsend.check_sum = _checksum(fsend.buffer);
+
+            if (num < 1)
+                return 0;
+            step++;
+            while (!success)
+            {
+                alarmHandlerGbn(1, serverAddr);
+                attempt++;
+                if (attempt > 1)
+                {
+                    printf("step %d done in %dth time\n", step, attempt);
+                }
+            }
+            filesize -= num;
+        } while (filesize > 0);
+    }
+    memset(&fsend, 0, sizeof(Frame));
+    fsend.length = 0;
+    sendto(sock, &fsend, sizeof(Frame),
+           MSG_CONFIRM, (struct sockaddr *)&serverAddr,
+           sizeof(serverAddr));
+    return 1;
+}
+
+int _read_file(int sock, FILE *f, struct sockaddr_in clientAddr)
+{
+    int num = 0;
+    int step = 0;
+    while (1)
+    {
+        Frame fread;
+        Frame fresponse;
+        socklen_t clientAddrLen = sizeof(clientAddr);
+        num = recvfrom(sock, &fread,
+                       sizeof(Frame), MSG_CONFIRM,
+                       (struct sockaddr *)&clientAddr, &clientAddrLen);
+        printf("recv %d %d\n", fread.length, num);
+        int ack = 1;
+        int sum = _checksum(fread.buffer);
+        int isBuffer = fread.frame_kind == SEQ;
+        int sumValid = fread.check_sum == sum;
+        int debug = 1; // rand() % 100 < 50;
+        int isValid = debug && isBuffer && sumValid;
+        if (!isValid)
+        {
+            ack = 0;
+        }
+        // if (rand() % 3 < 1) {
+        //     sleep(4);
+        // }
+        fresponse.ack = ack;
+        fresponse.frame_kind = ACK;
+        sendto(sock, &fresponse, sizeof(Frame), MSG_CONFIRM,
+               (struct sockaddr *)&clientAddr, clientAddrLen);
+        if (fread.length == 0)
+        {
+            return 0;
+        }
+        if (step == fread.seq_num)
+        {
+            fwrite(&fread.buffer[0], 1, fread.length, f);
+            step++;
+        }
+    };
+}
+
+void gbnClient(char *host, long port, FILE *fp)
+{
+
+    // char buffer[NET_BUF_SIZE];
+    // char *hello = "Hello from client";
+    struct sockaddr_in servaddr;
+
+    // Creating socket file descriptor
+    if ((sockfdc = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    struct hostent *hoost = gethostbyname(host);
+    unsigned int server_address = *(unsigned long *)hoost->h_addr_list[0];
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = server_address;
+
+    // int n, len;
+
+    // sendto(sockfd, (const char *)hello, strlen(hello),
+    // 	MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+    // 		sizeof(servaddr));
+    // printf("Hello message sent.\n");
+
+    // n = recvfrom(sockfd, (char *)buffer, NET_BUF_SIZE,
+    // 			MSG_WAITALL, (struct sockaddr *) &servaddr,
+    // 			&len);
+    // buffer[n] = '\0';
+    // printf("Server : %s\n", buffer);
+
+    _send_files(sockfdc, fp, servaddr);
+
+    close(sockfdc);
+}
+
+void gbnServer(char *iface, long port, FILE *fp)
+{
+
+    int sockfd;
+    // char buffer[NET_BUF_SIZE];
+    // char *hello = "Hello from server";
+    struct sockaddr_in servaddr, cliaddr;
+
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+
+    // Bind the socket with the server address
+    if (bind(sockfd, (const struct sockaddr *)&servaddr,
+             sizeof(servaddr)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // int n;
+    // int len;
+
+    // len = sizeof(cliaddr); //len is value/result
+
+    // n = recvfrom(sockfd, (char *)buffer, NET_BUF_SIZE,
+    // 			MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+    // 			&len);
+    // buffer[n] = '\0';
+    // printf("Client : %s\n", buffer);
+    // sendto(sockfd, (const char *)hello, strlen(hello),
+    // 	MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+    // 		len);
+    // printf("Hello message sent.\n");
+    _read_file(sockfd, fp, servaddr);
+}
+
+void gbn_server(char* iface, long port, FILE* fp) {
+    gbnServer(iface, port, fp);
+}
+
+void gbn_client(char* host, long port, FILE* fp) {
+  gbnClient(host, port, fp);
+}
---
+++ gobackn.h
@@ -0,0 +1,13 @@
+/*
+ *  Here is the starting point for your netster part.4 includes. Add the
+ *  appropriate comment header as defined in the code formatting guidelines.
+ */
+
+#ifndef P4_H
+#define P4_H
+
+/* add function prototypes */
+void gbn_server(char* iface, long port, FILE* fp);
+void gbn_client(char* host, long port, FILE* fp);
+
+#endif
