6b551eb4786561946f1053efd45a49fcce99bf2b
nashihu <ahmad.n.id@gmail.com>
2022-11-27 20:19:35 +0700

    mayan

--- gobackn.c
+++ gobackn.c
@@ -11,6 +11,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <signal.h>
+#include <errno.h>
 
 #define NET_BUF_SIZE 200
 #define N_WINDOW 5
@@ -18,6 +19,27 @@
 #define MSG_CONFIRM 0
 #endif
 
+int msleep(long msec)
+{
+    struct timespec ts;
+    int res;
+
+    if (msec < 0)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+
+    ts.tv_sec = msec / 10;
+    ts.tv_nsec = (msec % 10) * 1000000;
+
+    do
+    {
+        res = nanosleep(&ts, &ts);
+    } while (res && errno == EINTR);
+
+    return res;
+}
 typedef struct frame
 {
     char buffer[NET_BUF_SIZE];
@@ -36,9 +58,11 @@ int sockfdc;
 int success;
 int sequence;
 int lastSuccessIndex;
+long filesize;
+FILE *ff;
 Frame fsend[N_WINDOW];
 Frame fresponse;
-
+struct sockaddr_in serverAddr;
 int recvResponse(struct sockaddr_in serverAddr)
 {
     socklen_t serverAddrLen = sizeof(serverAddr);
@@ -52,6 +76,7 @@ int recvResponse(struct sockaddr_in serverAddr)
     return success;
 }
 
+int sdcLog = 0;
 void sendDataChunk(struct sockaddr_in serverAddr, int useAlarm, int i)
 {
     socklen_t serverAddrLen = sizeof(serverAddr);
@@ -60,11 +85,14 @@ void sendDataChunk(struct sockaddr_in serverAddr, int useAlarm, int i)
     int dapet = sendto(sockfdc, &fsend[i], sizeof(Frame),
                        MSG_CONFIRM, (struct sockaddr *)&serverAddr,
                        serverAddrLen);
-    printf("sent %d\n", dapet);
+    sdcLog++;
+    // printf("sent %d %zu %d\n", dapet, time(NULL), (sdcLog * NET_BUF_SIZE));
+    dapet++;
     if (useAlarm)
     {
         recvResponse(serverAddr);
     }
+    msleep(1);
 }
 
 int _checksum(char buffer[NET_BUF_SIZE])
@@ -92,14 +120,14 @@ void slide()
     }
 }
 
-void sendDataAlarm(int signum, long *filesize)
+void sendDataAlarm(int signum)
 {
     int i = lastSuccessIndex;
     do
     {
         memset(&fsend[i], 0, sizeof(fsend[i]));
-        size_t num = _min(*filesize, sizeof(fsend[i].buffer));
-        num = fread(fsend[i].buffer, 1, num, f);
+        size_t num = _min(filesize, sizeof(fsend[i].buffer));
+        num = fread(fsend[i].buffer, 1, num, ff);
         fsend[i].length = num;
         fsend[i].seq_num = sequence;
         sequence++;
@@ -107,28 +135,30 @@ void sendDataAlarm(int signum, long *filesize)
         fsend[i].check_sum = _checksum(fsend[i].buffer);
 
         if (num < 1)
-            return 0;
+            return;
         sendDataChunk(serverAddr, 0, i);
         filesize -= num;
+        printf("%d %lu %zu\n", i, filesize, num);
+        i++;
     } while (filesize > 0 && i < N_WINDOW); // TODO asumsi filesize aman
 }
 
 int _send_files(int sock, FILE *f, struct sockaddr_in serverAddr)
 {
     // sleep(2);
+    ff = f;
     fseek(f, 0, SEEK_END);
-    long filesize = ftell(f);
+    filesize = ftell(f);
     rewind(f);
     if (filesize == EOF)
         return 0;
 
     sequence = 0;
-    int i;
     lastSuccessIndex = 0;
     while (filesize > 0)
     {
 
-        sendDataAlarm(1, &filesize);
+        sendDataAlarm(1);
 
         int goback = 0;
         do
@@ -169,6 +199,7 @@ int _send_files(int sock, FILE *f, struct sockaddr_in serverAddr)
     return 1;
 }
 
+int rfLog = 0;
 int _read_file(int sock, FILE *f, struct sockaddr_in clientAddr)
 {
     int num = 0;
@@ -182,7 +213,8 @@ int _read_file(int sock, FILE *f, struct sockaddr_in clientAddr)
         num = recvfrom(sock, &fread,
                        sizeof(Frame), MSG_CONFIRM,
                        (struct sockaddr *)&clientAddr, &clientAddrLen);
-        printf("recv %d %d\n", fread.length, num);
+        rfLog++;
+        printf("recv %d %d %d\n", fread.length, num, (rfLog * NET_BUF_SIZE));
         filesize += fread.length;
         int ack = 1;
         int sum = _checksum(fread.buffer);
@@ -203,21 +235,16 @@ int _read_file(int sock, FILE *f, struct sockaddr_in clientAddr)
                (struct sockaddr *)&clientAddr, clientAddrLen);
         if (fread.length == 0)
         {
+            printf("done %d\n", filesize);
             return 0;
         }
-        if (step == fread.seq_num)
-        {
-            fwrite(&fread.buffer[0], 1, fread.length, f);
-            step++;
-        }
+        fwrite(&fread.buffer[0], 1, fread.length, f);
+        step++;
     };
-    printf("done %d\n", filesize);
 }
 
 void gbnClient(char *host, long port, FILE *fp)
 {
-
-    struct sockaddr_in servaddr;
 
     if ((sockfdc = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
@@ -225,16 +252,16 @@ void gbnClient(char *host, long port, FILE *fp)
         exit(EXIT_FAILURE);
     }
 
-    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&serverAddr, 0, sizeof(serverAddr));
 
     struct hostent *hoost = gethostbyname(host);
     unsigned int server_address = *(unsigned long *)hoost->h_addr_list[0];
 
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(port);
-    servaddr.sin_addr.s_addr = server_address;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+    serverAddr.sin_addr.s_addr = server_address;
 
-    _send_files(sockfdc, fp, servaddr);
+    _send_files(sockfdc, fp, serverAddr);
 
     close(sockfdc);
 }
